118a963364e4a15aaa71dc37fe14f387fc69d23a
Artemiy <los28.2001@mail.ru>
2022-04-27 01:08:14 +0300

    load time

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppInterceptor } from "./app.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
+  app.useGlobalInterceptors(new AppInterceptor());
   app.useStaticAssets(join(__dirname, '../../', 'public'));
   app.setBaseViewsDir(join(__dirname, '../../', 'views'));
   const hbs = require('hbs');
